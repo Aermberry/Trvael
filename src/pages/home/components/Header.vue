9bab3a84486d8720a885958d479bf127eaffe2e2
Salmonberry <sos2012happy@gmail.com>
2018-08-10 10:00:50 +0800

    update:webpack_iconfont

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
 <div class="header">
-  <div class="header-left">返回</div>
-  <div class="header-input">输入城市/景点/游玩主题</div>
-  <div class="header-right">城市</div>
+  <div class="header-left">
+    <div class="iconfont back-icon">&#xeacd;</div>
+  </div>
+  <div class="header-input">
+    <span class="iconfont">&#xeac6;</span>
+    输入城市/景点/游玩主题</div>
+  <div class="header-right">
+    城市
+    <span class="iconfont arrow-icon">&#xeac1;</span>
+    </div>
 </div>
 </template>
 <script>
@@ -12,25 +19,35 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+/* @代表src目录 */
+/* css引入其他css需要用@important */
+/* css中引入其他的css项并使用@符号需要在前面添加 ~ */
+@import '~styles/varibles.styl';
   .header
     display flex
     line-height .86rem
-    background #00bcd4
+    background $bgColor
     color #ffffff
     .header-left
       width .64rem
       float left
+      .back-icon
+        text-align center
+        font-size .4rem
     .header-input
       flex 1
       height .64rem
       line-height .64rem
       margin-top .12rem
       margin-left  .2rem
-      // background #ffffff
+      padding-left .2rem
+      background #ffffff
       border-radius .1rem
       color #cccccc
     .header-right
       width 1.24rem
       float right
       text-align center
+      .arrow-icon
+        font-size .24rem
 </style>

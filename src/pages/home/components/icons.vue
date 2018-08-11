9c27d19114ba6a23ec57917576a151b2e75efb02
Salmonberry <sos2012happy@gmail.com>
2018-08-11 14:53:16 +0800

    update:icons

--- src/pages/home/components/icons.vue
+++ src/pages/home/components/icons.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="icons">
-    <swiper>
+    <swiper :options="swiperOption">
       <swiper-slide v-for="(page, index) of pages" :key="index">
         <div class="icon" v-for="item of page" :key="item.id">
         <div class="icon-img">
@@ -17,6 +17,8 @@ export default {
   name: 'HomeIcons',
   data () {
     return {
+      swiperOption: {
+      },
       iconList: [{
         id: '0001',
         imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
